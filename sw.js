e38853e4dde1fc0fa76b98f6cdde81fe64c55b23
Mrinal Walia <waliamrinal@gmail.com>
2022-10-18 05:25:04 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-feb2e558c81915e5007e.js"
+    "url": "webpack-runtime-fb25465281ce31d539e2.js"
   },
   {
     "url": "framework-0f8de3526885dd073c5e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "469850512747728c68ba693214b775af"
+    "revision": "ebdbe5bf40eb316cb4d5fa5233f808a9"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-f277fb9b28e1661bc3bb.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "8357da17353a616d37f7598455399dae"
+    "revision": "b88f901bc1c24790c15d710f87aa5d53"
   },
   {
     "url": "polyfill-633d156253342d03a581.js"
